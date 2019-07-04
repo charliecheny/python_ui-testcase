b155e9a7a92f65897a99de844f0898356fd5dcc9
charliechen <m13916933780@163.com>
2019-07-04 20:17:47 +0800

    第四次提交

--- learning_space/hr-bot-turn_to_html/template_demo.py
+++ learning_space/hr-bot-turn_to_html/template_demo.py
@@ -1,43 +1,34 @@
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
+import os
 env = Environment(loader=FileSystemLoader("."),
                   autoescape=select_autoescape(['html', 'xml']))
 template = env.get_template("temple.html")
-# data = {
-#     "reportName":'哇哈哈',
-#     'users': [
-#         {
-#             'url': 123,
-#             'username': 56589
-#         },
-#         {
-#             'url': 'sdfaadfda',
-#             'username': "sdasdasda"
-#         }
-#     ]
-# }
-
+htmlname = datetime.datetime.now().strftime('%Y-%m-%d')
 data = {
+    'reportName':htmlname,
     'biaotou':[],
     'biaoneirong':[]
 }
-with open("./test.jtl", 'r', encoding='utf-8') as f:
+# 寻找当前文件夹路径下以.jtl结尾的文件，找不到就默认找test.jtl
+t = 'test.jtl'
+for file in os.listdir("."):
+    if file.endswith('.jtl'):
+        t = file
+        break
+with open(t, 'r', encoding='utf-8') as f:
     list1 = []
     for line in f:
         # 去除每一行的换行符
         line = line.strip('\n')
+        # 以逗号切分字符串
         split = line.split(',')
+        # 将每个切出来的字符串加入到列表中
         list1.append(split)
-        # print(len(split))
-        # print(split)
-    print(len(list1))
-    print(list1[0])
 data['biaotou'] = list1[0]
 data['biaoneirong'] = [i for i in list1[1:]]
-
-htmlname = datetime.datetime.now().strftime('%Y-%m-%d')
-
 template.stream(data=data).dump(f"{htmlname}.html")
+
 
 #learning_day01_15
